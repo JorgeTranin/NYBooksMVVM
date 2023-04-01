ae9214b91f763542c7cc07d4f9688f85d5ba8aad
jorge tranin <antoniotraninjorge@gmail.com>
2023-04-01 10:54:52 -0400

    Substituição da toobal principal para uma personalisada

--- app/src/main/java/com/nybooksmvvm/presentation/BooksActivity.kt
+++ app/src/main/java/com/nybooksmvvm/presentation/BooksActivity.kt
@@ -3,10 +3,17 @@ package com.nybooksmvvm.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nybooksmvvm.R
+import com.nybooksmvvm.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBooksBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_books)
+        binding = ActivityBooksBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbarMain.title = "Books"
+        setSupportActionBar(binding.toolbarMain)
+
     }
 }

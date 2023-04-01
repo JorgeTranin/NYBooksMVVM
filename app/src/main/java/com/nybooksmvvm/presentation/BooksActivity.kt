349f432a2b1f3cc8028d228917856cc9b39377b7
jorge tranin <antoniotraninjorge@gmail.com>
2023-04-01 10:33:00 -0400

    Estruturação do projeto e criação do README.md

---
+++ app/src/main/java/com/nybooksmvvm/presentation/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.nybooksmvvm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nybooksmvvm.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
